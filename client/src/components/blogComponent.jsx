37deaf98b695242b338f99571abcd3614eb8220f
Pourixa <pouria.footballer@gmail.com>
2026-08-01 11:54:37 +0200

    Build homepage blogs

---
+++ client/src/components/blogComponent.jsx
@@ -0,0 +1,10 @@
+import { BsHeartFill } from "react-icons/bs"
+import { Link } from "react-router"
+export default function Blog({blog}){
+    const date = new Date(blog.dateTime)
+    return <Link to={"/post/"+blog.id} className="blog">
+        <h1 data-fulltitle={blog.title}>{blog.title}</h1>
+        <p>{blog.text}</p>
+        <div><span><BsHeartFill/>{blog.likes}</span><span>{date.toLocaleDateString("en-GB")}</span></div>
+    </Link>
+}
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,18 +1,43 @@
 import "../styles/home.css"
 import hero from "../assets/hero.png"
 import BlogHolder from "./blogHolder"
+import { useEffect, useState } from "react"
+import Blog from "./blogComponent"
+
+function makeURL(path)
+{
+    return import.meta.env.VITE_API_URL + path
+}
+
+
+
 export default function Home() {
+
+    const [blogs,setBlogs] = useState([]) 
+    const [loading,setLoading] = useState(true)
+    useEffect(()=> {
+        async function getBlogs()  {
+            const data =await fetch(makeURL('/post'))
+            if (data.ok) {
+            console.log(data)
+            setBlogs(await data.json())
+            setLoading(false)
+            } else {
+                throw new Error()
+            }
+        }
+        getBlogs()
+    },[])
+
     return <main>
         <div id="hero">
             <img src={hero} alt="hero" />
             <a href="#blogsList" className="button">READ MORE</a>
         </div>
         <div id="blogsList">
-            <BlogHolder/>
-            <BlogHolder/>
-            <BlogHolder/>
-            <BlogHolder/>
-            <BlogHolder/>
+            {loading ? <><BlogHolder/><BlogHolder/><BlogHolder/><BlogHolder/><BlogHolder/> </>: blogs.map(blog => {
+                return <Blog blog={blog} key={blog.id}/>
+            })}
         </div>
     </main>
 }
